58a05ce8efb866bd420fb68fae62466257a9782c
waelkamira <127122203+waelkamira@users.noreply.github.com>
2024-09-25 02:36:00 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // missingSuspenseWithCSRBailout: false,
+  },
+
+  // distDir: 'build',
+  // output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;

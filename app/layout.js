b92c788244eb4eb130162d821806315025c71746
waelkamira <127122203+waelkamira@users.noreply.github.com>
2024-10-30 11:47:04 +0300

    change url randomly

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={rubik.className}>
-        <RandomizeDomain />
+        {/* <RandomizeDomain /> */}
         <BackButton />
         {/* إضافة كود Google Tag Manager (noscript) بعد بداية <body> */}
         <noscript>

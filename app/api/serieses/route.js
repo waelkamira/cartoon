6d613ab8b731ebd252d440201f1c9df652f55850
waelkamira <127122203+waelkamira@users.noreply.github.com>
2024-12-07 15:01:56 +0300

    fix reqests

--- app/api/serieses/route.js
+++ app/api/serieses/route.js
@@ -46,8 +46,7 @@ export async function GET(req) {
   const seriesName = searchParams.get('seriesName') || '';
   const planetName = searchParams.get('planetName') || '';
   const mostViewed = searchParams.get('mostViewed') || false; // تحويل القيمة إلى Boolean
-  console.log(typeof mostViewed);
-  console.log('mostViewed', mostViewed);
+
   try {
     let serieses;
 
@@ -74,13 +73,13 @@ export async function GET(req) {
       serieses = serieses.filter((series) => series.planetName === planetName);
     }
 
-    if (planetName && mostViewed === 'true') {
+    if (planetName && mostViewed) {
       serieses = serieses.filter((series) => series.mostViewed === 'True');
       // console.log('Serieses', serieses);
     }
 
     // ترتيب البيانات
-    if (mostViewed === 'true') {
+    if (mostViewed) {
       // console.log('Serieses before sorting:', serieses);
 
       serieses.sort((a, b) => {
